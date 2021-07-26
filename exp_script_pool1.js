f148fb1e4437375ea701a54b0f5a40152addc2bf
Ziwei Zhang <75584316+ZiweiZhang0304@users.noreply.github.com>
2021-07-27 01:06:21 +0800

    Add files via upload

--- exp_script_pool1.js
+++ exp_script_pool1.js
@@ -1033,7 +1033,7 @@ var sort_trial = {
     stimuli: sorting_stimuli,
     prompt: '<p>Drag and drop the 3 shapes in the boxes below in the order that you remember seeing them during the first part of the experiment.<br> Click next when you have arranged a group of three shapes in the order you remember them. <br>If you can’t remember a specific group of shapes, please make your best guess.</p>',
     sort_area_shape: "square",
-    stim_starts_inside:false,
+    stim_starts_inside:true,
     sort_area_height: 300,
     sort_area_width: 300
 };
