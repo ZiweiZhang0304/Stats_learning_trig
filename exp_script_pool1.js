d5809a94864124d5fe760fcd2851916d79321813
Ziwei Zhang <75584316+ZiweiZhang0304@users.noreply.github.com>
2021-07-31 04:48:33 +0800

    Add files via upload

--- exp_script_pool1.js
+++ exp_script_pool1.js
@@ -999,8 +999,14 @@ var twoAFC3 = {
 };
 
 //timeline.push(twoAFC1,twoAFC2);
-timeline.push(shuffle([twoAFC1,twoAFC2,twoAFC3])); //,twoAFC4,twoAFC5,twoAFC6
 
+var twoAFC = {
+    timeline: [twoAFC1,twoAFC2,twoAFC3], //,twoAFC4,twoAFC5,twoAFC6
+    //timeline_variables: first3_stimuli,
+    randomize_order: true,
+    repetitions: 1
+}
+timeline.push(twoAFC)
 
 /* -----Part 2: Recreate----- */
 //drag and drop
