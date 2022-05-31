993d9fff5bcfa5e5d25bc9a0609d80446489473d
Ziwei Zhang <75584316+ZiweiZhang0304@users.noreply.github.com>
2022-05-31 14:10:50 -0500

    Add files via upload

---
+++ img/Stim_followup/DD/followup_358da.js
@@ -0,0 +1,16 @@
+var followup_stimuli_DD = [
+ {
+   "trial_number": 1,
+   "DD_stim_list": "https://ziweizhang0304.github.io/Stats_learning_trig/img/Stim/FN_006_g.png,https://ziweizhang0304.github.io/Stats_learning_trig/img/Stim/FN_022_g.png,https://ziweizhang0304.github.io/Stats_learning_trig/img/Stim/FN_010_g.png",
+   "DD_trial_number": 1,
+   "attention_state": "fast",
+   "participant": "358da"
+ },
+ {
+   "trial_number": 2,
+   "DD_stim_list": "https://ziweizhang0304.github.io/Stats_learning_trig/img/Stim/FN_020_g.png,https://ziweizhang0304.github.io/Stats_learning_trig/img/Stim/FN_018_g.png,https://ziweizhang0304.github.io/Stats_learning_trig/img/Stim/FN_004_g.png",
+   "DD_trial_number": 2,
+   "attention_state": "slow",
+   "participant": "358da"
+ }
+]
