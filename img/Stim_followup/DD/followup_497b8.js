db1194864ee06d235adc75550042041660399fa2
ZiweiZhang0304 <zz112@uchicago.edu>
2022-06-01 10:19:44 -0500

    upload

---
+++ img/Stim_followup/DD/followup_497b8.js
@@ -0,0 +1,16 @@
+var followup_stimuli_DD = [
+ {
+   "trial_number": 1,
+   "DD_stim_list": "https://ziweizhang0304.github.io/Stats_learning_trig/img/Stim/FN_006_g.png,https://ziweizhang0304.github.io/Stats_learning_trig/img/Stim/FN_011_g.png,https://ziweizhang0304.github.io/Stats_learning_trig/img/Stim/FN_004_g.png",
+   "DD_trial_number": 1,
+   "attention_state": "fast",
+   "participant": "497b8"
+ },
+ {
+   "trial_number": 2,
+   "DD_stim_list": "https://ziweizhang0304.github.io/Stats_learning_trig/img/Stim/FN_021_g.png,https://ziweizhang0304.github.io/Stats_learning_trig/img/Stim/FN_017_g.png,https://ziweizhang0304.github.io/Stats_learning_trig/img/Stim/FN_014_g.png",
+   "DD_trial_number": 2,
+   "attention_state": "slow",
+   "participant": "497b8"
+ }
+]
