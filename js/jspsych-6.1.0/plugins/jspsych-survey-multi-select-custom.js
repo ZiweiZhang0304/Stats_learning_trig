a08257e933a4fcf31fc1c5357a554c59d38443ea
ZiweiZhang0304 <zz112@uchicago.edu>
2022-12-23 18:34:37 -0600

    Update jspsych-survey-multi-select-custom.js

--- js/jspsych-6.1.0/plugins/jspsych-survey-multi-select-custom.js
+++ js/jspsych-6.1.0/plugins/jspsych-survey-multi-select-custom.js
@@ -235,7 +235,7 @@ jsPsych.plugins['survey-multi-select'] = (function() {
 
         Object.assign(question_data, obje);
         if(val.length == 0){ has_response.push(false); } else { has_response.push(true); }
-        console.log(question_data)
+        //console.log(question_data)
         console.log(question_data['Q0'])
         console.log(Array.from(question_data['Q0'][0].id))
         console.log(Array.from(question_data['Q0'][0].['id']))
